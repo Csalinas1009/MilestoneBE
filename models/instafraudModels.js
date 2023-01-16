eaf2fb5b1ddfaa2c63e7e77ce672c3d99915836e
Christian Salinas <ChristianSalinas1009@gmail.com>
2023-01-16 15:19:40 -0500

    updated post route

--- models/instafraudModels.js
+++ models/instafraudModels.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose')
 
 const instaFraudSchema = new mongoose.Schema({
 
+    publicID: {
+        type: String
+    
+    },
+    
     imageUrl: {
         type: String,
         required: false
     }
-    
+
 },{timestamps: true})
 
 const Post = mongoose.model('instafraud', instaFraudSchema)
--- routes/instafraudRoutes.js
+++ routes/instafraudRoutes.js
@@ -4,70 +4,39 @@ const router = express.Router()
 const {uploadToCloudinary, removeFromCloudinary} = require('../services/cloudinary')
 const upload = require('../middleware/upload')
 
-//upload post
-router.post("/upload", upload.single("instafraudImage"), async (req, res) => {
+
+//upload to cloudinary
+router.post("/image/:id"), upload.single("image"), async (req, res) => {
     try {
-      //Upload Image to Cloudinary
-      const data = await uploadToCloudinary(req.file.path, "instafraud");
-      //Save Image Url and publiId ti the database
-      const savedImg = await Model.updateOne(
-        { _id: req.params.id },
-        {
-          $set: {
-            imageUrl: data.url,
-            publicId: data.public_id,
-          },
-        }
-      );
-  
-      res.status(200).send("user image uploaded with success!");
+        const data = await uploadToCloudinary(req.file.path, "instafraud");
+        const savedImg = await Model.updateOne(
+            {_id: req.params.id},
+            {
+                $set: {
+                    imageUrl: data.url,
+                    publicId: data.public_id
+                }
+            }
+        )
+        res.status(200).send("upload success")
     } catch (error) {
-      res.status(400).send(error);
+        res.status(400)
     }
-  });
+}
 
 
 //get all
 router.get("/", async (req,res)=>{
     try{
-        const pin = await Model.find();
-        res.status(200).json(pin);
+        const data = await Model.find();
+        res.status(200).json(data);
     }catch(err){
         console.log(err)
         res.status(500).send('err')
     }
 })
 
-//get by id
-router.get('/post/:id', async (req, res) => {
-    try {
-        const data = await Model.findById(req.params.id);
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
 
-//update by id
-router.patch("/:id", async (req,res)=>{
-    try{
-        const pin = await Model.findByIdAndUpdate( req.params.id, req.body);
-        res.status(200).json(pin);
-    }catch(err){
-        console.log(err)
-        res.status(500).send('err')
-    }
-})
 
-//delete by id
-router.delete("/:id", async (req,res)=>{
-    try{
-        const pin = await Model.findOneAndDelete({ _id: req.params.id });
-        res.status(200).json(pin);
-    }catch(err){
-        console.log(err)
-        res.status(500).send('err')
-    }
-})
 
 module.exports = router;
