d1b2ae8b3a353eee360314a45fc457c57b7e08e3
Christian Salinas <ChristianSalinas1009@gmail.com>
2023-01-17 17:48:56 -0500

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors');
 require('./db/mongoose')
 const imageRouter = require('./routers/images')
 
@@ -7,6 +8,7 @@ const app = express()
 const port = process.env.PORT || 4001
 
 app.use(express.json())
+app.use(cors())
 app.use(imageRouter)
 
 app.listen(port, () => {
