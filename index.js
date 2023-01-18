b3bc098470ca718256e0c1fd8be457408aa864d1
Christian Salinas <ChristianSalinas1009@gmail.com>
2023-01-17 21:01:03 -0500

    added /api on route

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4001
 
 app.use(express.json())
 app.use(cors())
-app.use('/upload', router)
+app.use('/api', router)
 
 app.listen(port, () => {
     console.log("server is up on port 4001")
